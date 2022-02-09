9fc36b508a78927337ba5b0b41ea3e1142682706
elsa lebas <lebas.elsa@gmail.com>
2022-02-09 13:20:53 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,70 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'ZH6901PX1J',
+  'd850a12faaa943ad0653aeffa19ad564'
+);
+
+const search = instantsearch({
+  indexName: '[Algolia Academy]crawler_lab',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+      <div>
+        <img src="{{image}}" align="left" style="width:100px" alt="{{name}}" />
+      <div class="hit-title">
+        {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+      </div>
+      <div class="hit-description">
+        {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+      </div>
+      <div class="hit-author">
+        {{#helpers.highlight}}{ "attribute": "author" }{{/helpers.highlight}}
+      </div>
+      <div class="hit-link">
+        <a href="{{url}}">Read More</a>
+      </div>
+    </div>
+`,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.addWidgets([
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#category',
+    attribute: 'category',
+    searchable: true,
+    limit: 3,
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#author',
+    attribute: 'author',
+    searchable: true,
+    limit: 3,
+    showMore: true,
+  }),
+
+  instantsearch.widgets.configure({
+    hitsPerPage: 10,
+  }),
+]);
+
+search.start();
